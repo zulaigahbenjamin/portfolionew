1f8a4b846aa3a80fe2cb1c6e4047d894d712d1c6
zulaigahbenjamin <130163047+zulaigahbenjamin@users.noreply.github.com>
2023-07-07 11:38:24 +0200

    al

--- src/views/TestimonialsView.vue
+++ src/views/TestimonialsView.vue
@@ -1,53 +1,69 @@
 <template>
-    <!-- <div>
-      <carousel>
+  <!-- <div>
+    <carousel>
 
-        <slide v-for="testimonial in testimonials" :key="testimonial.name">
-          <img {{ testimonial.image }}/>
-          <h3>{{ testimonial.name }}</h3>
-          <p>{{ testimonial.occupation }}</p>
-          <p>{{ testimonial.content }}</p>
-        </slide>
-      </carousel>
-    </div> -->
-<carousel>
-    <div v-if="testimonials" class="flex-container">
-      <CardComp v-for="testimonial of testimonials" :key="testimonial.id" :testimonial="testimonial"/>
-    </div>
-    <div v-else>Loading...</div>
+      <slide v-for="testimonial in testimonials" :key="testimonial.name">
+        <img {{ testimonial.image }}/>
+        <h3>{{ testimonial.name }}</h3>
+        <p>{{ testimonial.occupation }}</p>
+        <p>{{ testimonial.content }}</p>
+      </slide>
     </carousel>
-  </template>
-  
-  <script>
-  
-  import CardComp from '@/components/CardComp.vue'
- 
+  </div> -->
+<carousel>
+ <h1 class="welcome-text">Hear What Others Have To Say !</h1>
+  <div v-if="testimonials" class="flex-container">
+    <CardComp v-for="testimonial of testimonials" :key="testimonial.id" :testimonial="testimonial"/>
+  </div>
+  <div v-else>Loading...</div>
+  </carousel>
+</template>
 
-  export default {
-    components: { CardComp},
-    
-    computed : {
-      testimonials() {
-        return this.$store.state.testimonials
-      }
-    },
-    mounted() {
-      this.$store.dispatch('getTestimonials')
-    },
-  }
-  </script>
-  <style scoped>
+<script>
+
+import CardComp from '@/components/CardComp.vue'
+
+
+export default {
+  components: { CardComp},
+  
+  computed : {
+    testimonials() {
+      return this.$store.state.testimonials
+    }
+  },
+  mounted() {
+    this.$store.dispatch('getTestimonials')
+  },
+}
+</script>
+<style scoped>
 .flex-container {
 display:grid;
 grid-template-columns: repeat(2,1fr);
-
-
-}
-.pic {
-  width: 50px;
 }
 template{
-  text-decoration: none;
+text-decoration: none;
+}
+
+.welcome-text {
+  position: absolute;
+ margin-bottom: 50000px;
+  left: 30%;
+  transform: translate(-50%, -50%);
+  opacity: 0;
+  animation: slide-in 1s ease-in-out forwards;
+  z-index: 1;
+}
+
+@keyframes slide-in {
+  0% {
+    transform: translateX(-100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
 }
 </style>
-  
